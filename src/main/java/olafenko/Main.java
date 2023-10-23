d6a5d95690f47f9b925cbf4755ad73be946633b5
olafenko <olopikolo666@gmail.com>
2023-10-23 17:07:17 +0200

    First version

---
+++ src/main/java/olafenko/Main.java
@@ -0,0 +1,11 @@
+package olafenko;
+
+public class Main {
+    public static void main(String[] args) {
+
+        new Searching().run();
+
+
+
+    }
+}
---
+++ src/main/java/olafenko/Searching.java
@@ -0,0 +1,55 @@
+package olafenko;
+
+import java.util.*;
+
+public class Searching {
+
+    private ArrayList<String> checkedPersons = new ArrayList<>();
+    private HashMap<String, List<String>> persons = new HashMap<>();
+
+    public void run() {
+
+        preparing();
+
+        search("Me");
+
+    }
+
+    private void search(String name) {
+
+        Queue<String> personsToSearch = new LinkedList<>();
+        personsToSearch.add(name);
+
+        while (!personsToSearch.isEmpty()) {
+
+            String person = personsToSearch.poll();
+
+            if (!checkedPersons.contains(person)) {
+
+                if (isSellerOfMango(person)) {
+                    System.out.println("The seller of mango is: " + person);
+                    return;
+                } else {
+                    personsToSearch.addAll(persons.get(person));
+                    checkedPersons.add(person);
+                }
+            }
+
+            System.out.println(personsToSearch);
+        }
+
+    }
+    private boolean isSellerOfMango(String name) {
+        return name.endsWith("m");
+    }
+    private void preparing() {
+        persons.put("Me", List.of("Jakub","Konrad","Filip"));
+        persons.put("Konrad", List.of("Maciej", "Piotrek"));
+        persons.put("Jakub", List.of("Maciej", "Salem"));
+        persons.put("Filip", List.of("Ryszard"));
+        persons.put("Maciej",List.of());
+        persons.put("Piotrek", List.of());
+        persons.put("Salem",List.of());
+        persons.put("Ryszard",List.of());
+    }
+}
